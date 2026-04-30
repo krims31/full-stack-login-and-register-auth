faee7c25744a71a23657d08a92b2a481810c69de
krims31 <vegor405@gmail.com>
2026-04-30 19:35:24 +0300

    add login auth for deploy

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		origin: ['http://localhost:5173', 'https://full-stack-ai-p7ym.vercel.app'],
 		credentials: true
 	})
 )
