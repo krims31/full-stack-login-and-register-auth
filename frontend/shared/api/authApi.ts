21f14d858f3aca7fd9fa5acdb8eac330a1bf9ba2
krims31 <vegor405@gmail.com>
2026-05-01 11:31:40 +0300

    fix

--- frontend/shared/api/authApi.ts
+++ frontend/shared/api/authApi.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: import.meta.env.VITE_API_URL || "https://full-stack-ai-6uq7.onrender.com/api",
 	headers: {
 		'Content-Type': 'application/json'
 	},
-	withCredentials: true
+	withCredentials: false
 })
