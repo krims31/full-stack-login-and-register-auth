6b1b0964c58aaad862a7dae4473b6ddbed2ca6e3
krims31 <vegor405@gmail.com>
2026-05-03 15:57:41 +0300

    add icon and structure for userMenu dropdown

--- frontend/shared/lib/ui/UserMenu.tsx
+++ frontend/shared/lib/ui/UserMenu.tsx
@@ -1,6 +1,13 @@
-import { CircleUserRound } from 'lucide-react'
+import {
+	CircleUserRound,
+	Info,
+	LogOut,
+	MessageCircleMore,
+	Settings,
+	SunMoon,
+	UserRound
+} from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
-
 export default function UserMenu() {
 	const [isOpen, setIsOpen] = useState(false)
 	const ref = useRef<HTMLDivElement>(null)
@@ -36,32 +43,41 @@ export default function UserMenu() {
 				{isOpen && (
 					<div className="absolute left-15 -mt-75 w-40 bg-white rounded-xl p-2 border  border-black/10 shadow-lg hover:shadow-xl outline-0">
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							View Profile
+							<UserRound className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-4 left-11">View Profile</span>
 						</button>
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							Settings
-						</button>
-
-						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							Subscription
+							<Settings className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-13 left-11">Settings</span>
 						</button>
 
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							Support
+							<Settings className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-22.5 left-11">
+								Subscription
+							</span>
 						</button>
 
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							Community
+							<Info className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-32 left-11">Support</span>
 						</button>
 
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							Theme
+							<MessageCircleMore className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-41 left-11">Community</span>
+						</button>
+
+						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
+							<SunMoon className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-51 left-11">Theme</span>
 						</button>
 						<button
 							className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg text-red-500"
 							onClick={handleLogin}
 						>
-							Logout
+							<LogOut className="flex item-center relative z-10 right-2" />
+							<span className="absolute z-10 top-60 left-11">Logout</span>
 						</button>
 					</div>
 				)}
