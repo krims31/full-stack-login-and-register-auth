a97cf83eabc6a6d486cae3cb62eaf63df4633f32
krims31 <vegor405@gmail.com>
2026-05-11 17:45:58 +0300

    фикс storage.ts

--- backend/shared/Database/userStorage.ts
+++ backend/shared/Database/userStorage.ts
@@ -1,7 +1,8 @@
-import fs from 'fs'
+import fs, { existsSync } from 'fs'
 import path from 'path'
 
 const DB_PATH = path.join(process.cwd(), 'db.json')
+console.log('DB_PATH', DB_PATH)
 
 interface User {
 	id: number
@@ -11,8 +12,17 @@ interface User {
 	createdAt: string | Date
 }
 
+const initDB = () => {
+	if (!existsSync(DB_PATH)) {
+		console.log("DB_PATH doesn't exist, creating...")
+		fs.writeFileSync(DB_PATH, JSON.stringify({ users: [] }, null, 2))
+		console.log('DB file created at: ', DB_PATH)
+	}
+}
+
 export const userStorage = {
 	getAll: (): User[] => {
+		initDB()
 		if (!fs.existsSync(DB_PATH)) {
 			fs.writeFileSync(DB_PATH, JSON.stringify({ users: [] }))
 			return []
@@ -32,6 +42,7 @@ export const userStorage = {
 	},
 
 	save: (user: User) => {
+		initDB()
 		const currentData = JSON.parse(fs.readFileSync(DB_PATH, 'utf-8'))
 		currentData.users.push(user)
 		fs.writeFileSync(DB_PATH, JSON.stringify(currentData, null, 2))
