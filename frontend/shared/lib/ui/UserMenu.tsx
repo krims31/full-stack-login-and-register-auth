dd189a4fc41ea5600d6c7f92e5cda8764a74f6f7
krims31 <vegor405@gmail.com>
2026-05-13 13:31:45 +0300

    добавил полоску разделитель и исправил иконку subscription

--- frontend/shared/lib/ui/UserMenu.tsx
+++ frontend/shared/lib/ui/UserMenu.tsx
@@ -3,6 +3,7 @@ import {
 	Info,
 	LogOut,
 	MessageCircleMore,
+	Podcast,
 	Settings,
 	SunMoon,
 	UserRound
@@ -46,13 +47,18 @@ export default function UserMenu() {
 							<UserRound className="flex item-center relative z-10 right-2" />
 							<span className="absolute z-10 top-4 left-11">View Profile</span>
 						</button>
+
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
 							<Settings className="flex item-center relative z-10 right-2" />
 							<span className="absolute z-10 top-13 left-11">Settings</span>
 						</button>
 
+						<div>
+							<div className="h-0.5 bg-gray-300 shadow-sm border-t border-white"></div>
+						</div>
+
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
-							<Settings className="flex item-center relative z-10 right-2" />
+							<Podcast className="flex item-center relative z-10 right-2" />
 							<span className="absolute z-10 top-22.5 left-11">
 								Subscription
 							</span>
@@ -63,6 +69,10 @@ export default function UserMenu() {
 							<span className="absolute z-10 top-32 left-11">Support</span>
 						</button>
 
+						<div>
+							<div className="h-0.5 bg-gray-300 shadow-sm border-t border-white"></div>
+						</div>
+
 						<button className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg">
 							<MessageCircleMore className="flex item-center relative z-10 right-2" />
 							<span className="absolute z-10 top-41 left-11">Community</span>
@@ -72,6 +82,10 @@ export default function UserMenu() {
 							<SunMoon className="flex item-center relative z-10 right-2" />
 							<span className="absolute z-10 top-51 left-11">Theme</span>
 						</button>
+
+						<div>
+							<div className="h-0.5 bg-gray-300 shadow-sm border-t border-white"></div>
+						</div>
 						<button
 							className="w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg text-red-500"
 							onClick={handleLogin}
